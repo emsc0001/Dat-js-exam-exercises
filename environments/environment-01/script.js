d8ea7f00854442599764e35f72d72920a4be8cef
Emil Schlichter <emilschlichter@gmail.com>
2023-05-31 15:28:04 +0200

    s

--- environments/environment-01/script.js
+++ environments/environment-01/script.js
@@ -1,2 +1,17 @@
 "use strict";
 
+window.addEventListener("load", start);
+
+import users from "./users.json" 
+
+let users;
+
+fetch('./users.json')
+  .then(response => response.json())
+  .then(data => {
+    users = data;
+    console.log(users); // Udskriv JSON-dataene til konsollen
+  })
+  .catch(error => {
+    console.log('Fejl ved hentning af JSON-data:', error);
+  });
--- environments/environment-02/script.js
+++ environments/environment-02/script.js
@@ -1,2 +1,38 @@
 "use strict";
 
+window.addEventListener("load", start);
+
+function start () {
+    console.log("hi");
+    document.querySelector("#create-form").addEventListener("submit, submitForm");
+}
+
+const animals = []
+
+function submitForm (event) {
+    event.prevetdefault();
+
+    const form = event.target;
+    const name = form.name.value;
+    const age = form.age.value;
+    const type = form.type.value;
+    showAnimals (name, age, type);
+}
+
+function showAnimals (animals) {
+    document.querySelector("students-table-body").innerHTML = "";
+    students.sort ((a, b) => a.name.localeCompare (b.name));
+    students.filter((animals) => animals.age <= 18).forEach(showAnimal)
+
+
+}
+
+function showAnimal (animal) {
+    document.querySelector("students-table-body").insertAdjacentHTML
+    ("beforeend",
+    `<tr>
+    <th>${Name}</th>
+    <th>${Type}</th>
+    <th>${Age}</th>
+    </tr>`);
+}
