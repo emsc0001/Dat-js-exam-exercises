71b552267e4510a0cb76f78688698689dd0dd910
Emil Schlichter <emilschlichter@gmail.com>
2023-06-06 20:05:11 +0200

    Update script.js

--- environments/environment-02/script.js
+++ environments/environment-02/script.js
@@ -1,116 +1,47 @@
 "use strict";
 
+window.addEventListener("load", start);
 
-const animals = [
-  {
-    name: "Leo",
-    type: "Lion",
-    age: 5,
-  },
-  {
-    name: "Max",
-    type: "Dog",
-    age: 3,
-  },
-  {
-    name: "Whiskers",
-    type: "Cat",
-    age: 2,
-  },
-];
+function start() {
+  console.log("hi");
+  document.getElementById("create-form").addEventListener("submit", createAnimal);
+}
+
+const animals = [];
+
+function createAnimal(event) {
+  event.preventDefault();
+
+  const nameInput = document.getElementById("animal-name").value;
+  const typeInput = document.getElementById("animal-type").value;
+  const ageInput = document.getElementById("animal-age").value;
+
+  const animal = {
+    name: nameInput,
+    type: typeInput,
+    age: ageInput
+  };
+
+  animals.push(animal);
+  console.log(animals);
+  displayAnimals();
+  event.target.reset();
+}
+
 
 function displayAnimals() {
   const animalTableBody = document.querySelector("#list-container tbody");
   animalTableBody.innerHTML = "";
 
-  // Sort animals by age
-  const sortedAnimals = animals.sort((a, b) => a.age - b.age);
+  const sortedAnimals = animals.sort((a, b) => a.name.localeCompare(b.name));
 
   sortedAnimals.forEach((animal) => {
     const row = document.createElement("tr");
-    const nameCell = document.createElement("td");
-    const typeCell = document.createElement("td");
-    const ageCell = document.createElement("td");
-
-    nameCell.textContent = animal.name;
-    typeCell.textContent = animal.type;
-    ageCell.textContent = animal.age;
-
-    row.appendChild(nameCell);
-    row.appendChild(typeCell);
-    row.appendChild(ageCell);
+    row.innerHTML = `
+      <td>${animal.name}</td>
+      <td>${animal.type}</td>
+      <td>${animal.age}</td>
+    `;
     animalTableBody.appendChild(row);
   });
 }
-
-function createAnimal(event) {
-  event.preventDefault();
-
-  const nameInput = document.getElementById("animal-name");
-  const typeInput = document.getElementById("animal-type");
-  const ageInput = document.getElementById("animal-age");
-
-  const name = nameInput.value;
-  const type = typeInput.value;
-  const age = parseInt(ageInput.value);
-
-  if (name && type && !isNaN(age)) {
-    const newAnimal = {
-      name,
-      type,
-      age,
-    };
-
-    animals.push(newAnimal);
-    displayAnimals();
-
-    nameInput.value = "";
-    typeInput.selectedIndex = 0;
-    ageInput.value = "";
-  }
-}
-
-const createForm = document.getElementById("create-form");
-createForm.addEventListener("submit", createAnimal);
-
-// Call the function to display the animals on page load
-displayAnimals();
-
-// window.addEventListener("load", start);
-
-// function start () {
-//     console.log("hi");    
-//     document.querySelector("#create-form").addEventListener("submit, submitForm");
-// }
-
-
-
-// function submitForm (event) {
-//     event.prevetdefault();    
-
-//     const form = event.target;
-//     const name = form.name.value;
-//     const age = form.age.value;
-//     const type = form.type.value;
-//     showAnimals (name, age, type);
-// }
-
-// function showAnimals (animals) {
-//     document.querySelector("students-table-body").innerHTML = "";    
-//     students.sort ((a, b) => a.name.localeCompare (b.name));
-//     students.filter((animals) => animals.age <= 18).forEach(showAnimal)
-
-
-// }
-
-// function showAnimal (animal) {
-//     document.querySelector("students-table-body").insertAdjacentHTML    
-//     ("beforeend",
-//     `<tr>
-//     <th>${Name}</th>
-//     <th>${Type}</th>
-//     <th>${Age}</th>
-//     </tr>`);
-// }
-
-
