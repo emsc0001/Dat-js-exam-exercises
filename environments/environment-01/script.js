0b40ca0f75dd387f56ea2bfea379dcbf386d0654
Emil Schlichter <emilschlichter@gmail.com>
2023-06-09 23:39:23 +0200

    67

--- environments/environment-01/script.js
+++ environments/environment-01/script.js
@@ -4,46 +4,25 @@ window.addEventListener("load", start)
 
 let users
 
-const listSelector = document.querySelector("#userlist");
-
 async function start () {
   users = await getData()
-  console.log(users);
+  console.log(users)
   showUsers()
 }
 
-async function getData () {
+async function getData() {
   const data = await fetch ("users.json")
-  return data.json()
+  return data.json();
 }
 
-function showUsers () {
+function showUser () {
   listSelector.textContent = ""
   users.forEach(showUser)
 }
 
-function showUser (user) {
+function showUsers(user)
   const userHTML = /*HTML*/ `
-  <li>Name: ${user.name} - ${user.role}</li>`
+  <li>${user.name} - ${user.active} </li>`
   listSelector.insertAdjacentHTML("beforeend", userHTML)
-  countUsers(users);
-}
+  
 
-function countUsers(list) {
-  let admins = 0
-  let users = 0
-  let guests = 0
-
-  for(const user of list){
-    if (user.role === "admin"){
-      admins++
-    } else if (user.role === "user") {
-      users++;
-    } else if (user.role === "guest") {
-      guests++;
-    }
-  }
-    document.querySelector("#admin-count").textContent = admins
-    document.querySelector("#user-count").textContent = users
-    document.querySelector("#guest-count").textContent = guests
-}
